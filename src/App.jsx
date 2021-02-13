302b61c8087bc2d36425ab95622279a470f0caf2
Max <qutbiddinmaxmudov@mail.ru>
2021-02-13 13:07:43 +0500

    end 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import "./index.scss";
 
 import { Header } from "./components";
@@ -5,11 +8,18 @@ import { Home, Cart } from "./pages";
 import { Route } from "react-router-dom";
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/db.json")
+      .then(({ data }) => setPizzas(data.pizzas));
+  });
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home items={pizzas} />} />
         <Route exact path="/cart" component={Cart} />
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Router>
+    <App />
+  </Router>,
+  // </React.StrictMode>
   document.getElementById("root")
 );
