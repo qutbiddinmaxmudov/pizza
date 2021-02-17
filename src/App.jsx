ee6ec2316c3ac5095e1f17d0797a7519cc89e4e3
Max <qutbiddinmaxmudov@mail.ru>
2021-02-17 16:35:01 +0500

    part-7 1-02

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 import axios from "axios";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { setPizzas } from "./redux/actions/pizzas";
 
@@ -10,52 +10,23 @@ import "./index.scss";
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 
-// function App() {
-//   useEffect(() => {
-//     axios
-//       .get("http://localhost:3000/db.json")
-//       .then(({ data }) => setPizzas(data.pizzas));
-//   });
-//   return (
-//     <div className="wrapper">
-//       <Header />
-//       <div className="content">
-//         <Route exact path="/" render={() => <Home items={pizzas} />} />
-//         <Route exact path="/cart" component={Cart} />
-//       </div>
-//     </div>
-//   );
-// }
-
-class App extends React.Component {
-  componentDidMount() {
+function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
     axios
       .get("http://localhost:3000/db.json")
-      .then(({ data }) => this.props.setPizzas(data.pizzas));
-  }
-  render() {
-    return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Route
-            exact
-            path="/"
-            render={() => <Home items={this.props.items} />}
-          />
-          <Route exact path="/cart" component={Cart} />
-        </div>
+      .then(({ data }) => dispatch(setPizzas(data.pizzas)));
+  }, [dispatch]);
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/cart" component={Cart} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return { items: state.pizzas.items };
-};
-
-const mapDispatchToProps = {
-  setPizzas,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import { Categories, PizzaBlock, SortPopup } from "../components";
 
-function Home({ items }) {
+function Home() {
+  const items = useSelector((state) => state.pizzas.items);
   return (
     <div className="container">
       <div className="content__top">
@@ -35,9 +37,7 @@ function Home({ items }) {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {items.map((item) => (
-          <PizzaBlock key={item.id} {...item} />
-        ))}
+        {items && items.map((item) => <PizzaBlock key={item.id} {...item} />)}
       </div>
     </div>
   );
